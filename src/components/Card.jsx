70a1f723c17eaabe2412882c53aadb03a65c0787
suryacodess <helloosuryaprakash@gmail.com>
2025-10-17 13:08:44 +0530

    destructuring card props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,13 @@
-export default function Card({ img, title, text }) {
+export default function Card(content) {
+  const { img, title, text } = content;
   return (
     <div className="card">
       <div className="card-img h-[200px] w-full">
-        <img src={img} alt={title} className="w-full h-full object-cover rounded-2xl"/>
+        <img
+          src={img}
+          alt={title}
+          className="w-full h-full object-cover rounded-2xl"
+        />
       </div>
       <div className="card-body p-2">
         <h2 className="text-2xl font-bold">{title}</h2>
