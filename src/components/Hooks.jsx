b0667bc86af111a64ad4a6411c8e3b9e803e248c
suryacodess <helloosuryaprakash@gmail.com>
2025-10-18 18:24:27 +0530

    added useeffect hook and called a dummy with load more btn functionality

--- src/components/Hooks.jsx
+++ src/components/Hooks.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Hooks() {
   let [count, setCount] = useState(0);
@@ -11,7 +11,9 @@ export default function Hooks() {
   const [valueInput, setValueInput] = useState("");
   const [valueArr, setValueArr] = useState([1, 2, 3, 4, 5, 6]);
   const [updateValueShow, setUpdateValueShow] = useState(false);
-  const [editValueIndex, setEditValueIndex] = useState();
+  const [editValueIndex, setEditValueIndex] = useState({});
+  const [data, setData] = useState([]);
+  const [dataCount, setDataCount] = useState(8);
 
   const counter = (type) => {
     if (type === "increase") {
@@ -74,6 +76,23 @@ export default function Hooks() {
     setUpdateValueShow(false);
     setValueInput("");
   };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetchApi = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/"
+        );
+        if (!fetchApi.ok) throw new Error("Something went wrong");
+        const response = await fetchApi.json();
+        setData(response);
+      } catch (error) {
+        console.log("Fetch failed", error);
+        setData([]);
+      }
+    };
+    getData();
+  }, []);
 
   return (
     <div className="hooks max-w-[1200px] mx-auto px-2 pt-10">
@@ -167,6 +186,41 @@ export default function Hooks() {
             </div>
           );
         })}
+      </div>
+
+      <div className="pt-5">
+        <h2 className="text-2xl font-bold poppins">UseEffect</h2>
+        {data.length > 0 ? (
+          <div className="">
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 pt-4">
+              {data.slice(0, dataCount).map((item, index) => {
+                return (
+                  <div className="bg-purple-500 p-5 rounded-2xl" key={item.id}>
+                    <h2>{item.id}</h2>
+                    <h2>{item.title}</h2>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="text-center pt-4">
+              <button
+                className="px-4 py-2 bg-purple-700 text-white rounded-[4px] cursor-pointer"
+                onClick={() => {
+                  dataCount < data.length && setDataCount((prev) => prev + 20);
+                }}
+                style={
+                  dataCount >= data.length
+                    ? { cursor: "not-allowed" }
+                    : { cursor: "pointer" }
+                }
+              >
+                Load more
+              </button>
+            </div>
+          </div>
+        ) : (
+          <h1>Loading</h1>
+        )}
       </div>
     </div>
   );
